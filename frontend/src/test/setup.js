6ea13815d4408299981486525fb8d5892978072f
Caspian Almerud <almerudcaspian@gmail.com>
2025-08-29 10:49:25 +0200

    Removed unused files

--- frontend/src/test/setup.js
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom';
-import { setupGlobalMocks } from './utils/globalMocks';
-
-// Initialize all global mocks
-setupGlobalMocks(); 
--- frontend/src/test/utils/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Test utilities exports
- * Centralized exports for all test-related utilities
- */
-
-export * from './globalMocks';
-export * from './testHelpers';
